b198629b3e4ccf1452ba672891c042561b8cbb25
Chris Wolfe <chriswwolfe@gmail.com>
2017-06-07 11:38:48 -0500

    better

--- src/az_manifold/core.clj
+++ src/az_manifold/core.clj
@@ -14,34 +14,27 @@
     (Thread/sleep 2000)
     {arg now}))
 
-(def output (ms/stream))
+(def max-concurrency 3)
 
 (defn process
   [the-nums out]
-  ;; fire off x deferreds
-  (prn the-nums)
+  ;; fire off max-concurrency deferreds
   (md/chain
    (apply md/zip
           (map
            (fn [arg]
-             (prn arg)
+             ;;(prn arg)
              (md/future (expensive! arg)))
            the-nums))
    (fn [vals]
+     (println "got:" vals)
      (ms/put-all! out vals))))
-
-(defn staying-alive
-  "Create a background thread to keep the entire process running."
-  []
-  (.start (Thread. (fn [] (.join (Thread/currentThread))) "staying alive")))
-
-(def max-concurrency 20)
 
 (defn -main
   [& args]
-  (staying-alive)
-  (ms/consume-async #(process % output) (->>
-                                         (ms/->source (take 60 (range)))
-                                         (ms/batch max-concurrency)))
-  (ms/consume-async #(pp/pprint %) output))
-  #_(pp/pprint (ms/stream->seq output))
+  (let [output (ms/stream)
+        close (ms/consume-async #(process % output) (->>
+                                                     (ms/->source (take 25 (range 1 100 1)))
+                                                     (ms/batch max-concurrency)))
+        finished (doall (ms/stream->seq output))]
+    (pp/pprint finished)))
