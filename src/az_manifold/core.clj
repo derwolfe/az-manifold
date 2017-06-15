edb27cd50afa54006c148c1082aa5b98df79ec8d
Chris Wolfe <chriswwolfe@gmail.com>
2017-06-15 08:59:05 -0500

    simpler

--- src/az_manifold/core.clj
+++ src/az_manifold/core.clj
@@ -15,8 +15,7 @@
 (def max-concurrency 3)
 
 (defn process
-  [the-nums out batch-size]
-  ;; fire off max-concurrency deferreds
+  [the-nums out]
   (prn "batch!")
   (md/chain
    (apply md/zip
@@ -25,24 +24,7 @@
              (prn arg)
              (md/future (expensive! arg)))
            the-nums))
-   (fn [vals]
-     (let [more? (= (count the-nums) batch-size)
-           ret (ms/put-all! out vals)]
-       (when (not more?)
-         (ms/close! out))
-       ret))))
-
-(defn using-consume-async
-  []
-  (let [output (ms/stream)
-        vals (concat (take 25 (range 1 100 1)) nil)
-        batches (->>
-                 (ms/->source vals)
-                 (ms/batch max-concurrency))
-        _ (ms/consume-async #(process % output max-concurrency) batches)
-        finished (vec (ms/stream->seq output))]
-    (pp/pprint finished)
-    (prn "consume async DONE")))
+   #(ms/put-all! out %)))
 
 (defn using-connect-via
   []
@@ -51,17 +33,7 @@
         batches (->>
                  (ms/->source vals)
                  (ms/batch max-concurrency))
-        p2 (fn [val out]
-             (prn "batch!")
-             (md/chain
-              (apply md/zip
-                     (map
-                      (fn [arg]
-                        (prn arg)
-                        (md/future (expensive! arg)))
-                      val))
-              #(ms/put-all! out %)))
-        _ (ms/connect-via batches #(p2 % output) output)
+        _ (ms/connect-via batches #(process % output) output)
         finished (doall (ms/stream->seq output))]
     (pp/pprint finished)
     (prn "connect-via DONE")))
