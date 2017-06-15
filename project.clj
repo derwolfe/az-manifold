cd23f7dff5c4db37141646b9a9b4e0e22acbbf52
Chris Wolfe <chriswwolfe@gmail.com>
2017-06-15 09:03:26 -0500

    wait until later

--- project.clj
+++ project.clj
@@ -4,9 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [amazonica "0.3.86"]
-                 [manifold "0.1.7-alpha5"]
-                 [environ "1.1.0"]]
+                 [manifold "0.1.7-alpha5"]]
   :plugins [[lein-environ "1.0.2"]]
   :main ^:skip-aot az-manifold.core
   :target-path "target/%s"
